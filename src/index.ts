99a08876a8c9bec4f8b9235100f6c73c36487437
Shiro2938 <hmegxva@gmail.com>
2025-02-12 21:49:12 +0700

    add: cors

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
 import express from 'express'
 import authRouter from './routes/auth.routes'
 import { defaultErrorHandler } from './middlewares/error.middleware'
-import databaseSerice from './services/database.service'
-// import { defaultErrorHandler } from './middlewares/error.middleware'
+import databaseService from './services/database.service'
+import { config } from 'dotenv'
+import cors from 'cors'
+
+config()
 const app = express()
 const port = 8081
 
+app.use(
+  cors({
+    origin: process.env.HOST_FE || 'http://localhost:3000', // Thêm fallback nếu HOST_FE không hoạt động
+    credentials: true, // Nếu có dùng cookies hoặc session
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Chỉ định các phương thức HTTP cho phép
+    allowedHeaders: ['Content-Type', 'Authorization'] // Chỉ định các headers cho phép
+  })
+)
+
 app.use(express.json())
 
-databaseSerice.connect()
+databaseService.connect()
 
 app.use('/auth', authRouter)
 app.use(defaultErrorHandler)
